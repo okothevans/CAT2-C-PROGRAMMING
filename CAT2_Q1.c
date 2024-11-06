45a871d25df1ebe65540a785d81e94657075ac05
Okoth Evans Okumu-CT102/G/23443/24 <evansokoth641@gmail.com>
2024-11-06 15:54:52 +0000

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,33 @@
+/*
+Evans Okoth Okumu
+CT102/G/23443/24
+*/
+
+#include<stdio.h> 
+#include<string.h> 
+
+struct employee{
+	char name[25],dept[20],email[50];
+	int id;
+	float salary;
+	
+	
+}employee1;
+
+int main(){
+	strcpy(employee1.name,"John Doe");
+	strcpy(employee1.dept,"Human Resources");
+	strcpy(employee1.email,"john.doe@company.com");
+	employee1.id=12345;
+	employee1.salary=55000.50;
+	
+	printf("Name:%s\n",employee1.name);
+	printf("Department:%s\n",employee1.dept);
+	printf("Email:%s\n",employee1.email);
+	printf("Id No:%d\n",employee1.id);
+	printf("Salary:%.2f\n",employee1.salary);
+	
+	
+	
+	return 0;
+}
