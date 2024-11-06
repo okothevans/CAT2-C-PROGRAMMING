e0459d0dafd5457e3b079d1a795cc16464af694c
Okoth Evans Okumu-CT102/G/23443/24 <evansokoth641@gmail.com>
2024-11-06 16:04:45 +0000

    Add files via upload

---
+++ CAT2_Q3.c
@@ -0,0 +1,44 @@
+/*
+EVANS OKOTH OKUMU
+CT102/G/23443/24
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    float hoursworked, hourlywage, grosspay, tax, netpay;
+    float overtimehours = 0;
+    
+    printf("Enter number of hoursworked in a week: ");
+    scanf("%f", &hoursworked);
+    
+    printf("Enter the hourlywage: ");
+    scanf("%f", &hourlywage);
+    
+    //Calculate overtime
+    if (hoursworked > 40) {
+   
+        overtimehours = hoursworked - 40;
+        grosspay = (40 * hourlywage) + (overtimehours * hourlywage * 1.5);
+    } else {
+       
+        grosspay = hoursworked * hourlywage;
+    }
+
+    //Calculate trexrate
+    if (grosspay <= 600) {
+        tax = grosspay * 0.15; 
+    } else {
+        tax = (600 * 0.15) + ((grosspay - 600) * 0.20);
+    }
+
+  
+    netpay = grosspay - tax;
+  
+    printf("Gross Pay: $%.3f\n", grosspay);
+    printf("Taxes: $%.3f\n", tax);
+    printf("Net Pay: $%.3f\n", netpay);
+
+    return 0;
+}
